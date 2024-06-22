471830b4b4ac0f197facdb93de0f2f996aa835e7
NITIN PANDEY <144930015+Nitin192005@users.noreply.github.com>
2024-06-22 18:46:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+const express = require('express') // == import express from "express"
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/nitin', (req, res) => {
+  res.send('Hello sir')
+})
+app.get('/nikhil', (req, res) => {
+  res.send('Hello 2 in 1')
+})
+
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
